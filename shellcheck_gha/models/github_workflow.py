cf92d09248e6cbe3b6df400cb6dcf53711549c27
Mikail Kocak <mikail.kocak@saleor.io>
2024-03-22 12:29:51 +0100

    feature: add support for composite actions
    This adds parsing and handling logic to iterate over all shell script of GitHub composite actions (https://docs.github.com/en/actions/creating-actions/creating-a-composite-action).

--- shellcheck_gha/models/github_workflow.py
+++ shellcheck_gha/models/github_workflow.py
@@ -1,6 +1,21 @@
-from typing import Optional
+import dataclasses
+from pathlib import Path
+from typing import Optional, Generator
 
 from pydantic import BaseModel, Field, computed_field
+
+
+class UnknownYAMLFileException(Exception):
+    """
+    An exception raised when the parsed YAML file does not appear to be either
+    a GitHub Workflow, or a GitHub Composite Action.
+    """
+
+
+@dataclasses.dataclass
+class ShellSnippet:
+    yaml_path: Path
+    step_specs: "GitHubStepRun"
 
 
 class GitHubStepRun(BaseModel):
@@ -23,9 +38,43 @@ class GitHubStepRun(BaseModel):
         return self.run.splitlines(keepends=False)
 
 
-class GitHubJob(BaseModel):
+class GitHubSteps(BaseModel):
     steps: list[GitHubStepRun] = Field(default_factory=list)
 
+    def iter_shell_scripts(
+        self, yaml_path: Path
+    ) -> Generator[ShellSnippet, None, None]:
+        for step in self.steps:
+            # Skip if 'run' is not defined as it means it is not a shell script
+            # but rather an action reference (e.g., 'actions/checkout' step).
+            if not step.run:
+                continue
 
-class GitHubWorkflow(BaseModel):
-    jobs: dict[str, GitHubJob]
+            yield ShellSnippet(
+                yaml_path=yaml_path,
+                step_specs=step,
+            )
+
+
+class GitHubYAML(BaseModel):
+    """Pydantic model for both a GitHub Workflow, and a Composite GitHub Action."""
+
+    jobs: Optional[dict[str, GitHubSteps]] = None
+    runs: Optional[GitHubSteps] = None
+
+    def iter_shell_scripts(
+        self, yaml_path: Path
+    ) -> Generator[ShellSnippet, None, None]:
+        if self.jobs:
+            for job in self.jobs.values():
+                for snippet in job.iter_shell_scripts(yaml_path):
+                    yield snippet
+        elif self.runs:
+            for snippet in self.runs.iter_shell_scripts(yaml_path):
+                yield snippet
+        else:
+            # This code should never be reached unless a non-GitHub workflow or action
+            # YAML file was provided to the CLI.
+            raise UnknownYAMLFileException(
+                "The YAML file should contain either 'jobs' or 'runs'."
+            )
